63bde6c57a0a113c8d69c345a0bc4c6d4ed9df0f
Rahul Agrawal <rahulagr2000@gmail.com>
2016-04-15 14:56:59 -0700

    rule based system
    distance queries recognizer

---
+++ RuleBasedSystem/rule.py
@@ -0,0 +1,41 @@
+'''
+Program to process the query through a set of rules and  determine the slots
+to be filled in the Postgre SQL Query
+'''
+
+import sys
+
+print "GEOGRAPHIC QUESTION ANSWERING SYSTEM"
+print "------------------------------------\n"
+print "Loading Query from file... "
+
+#filePath=sys.argv[1]
+
+#WINDOWS: filePath="C:\Users\rahul\Desktop\hindiCorpus.txt"
+#LINUX: filePath="C:/Users/rahul/Desktop/hindiCorpus.txt"
+
+filePath="hindiQuery.txt"
+
+input_file_handler= open(filePath,"r")
+output_file_handler= open("nlpOutput.txt","w")
+
+#query=input_file_handler.readline().strip().split()
+query=input_file_handler.readline().strip().split()
+print query
+input_file_handler.close()
+
+for i in query:
+	output_file_handler.write(i)
+
+#Finding Synonyms
+input_file_handler= open("../synonyms/distance.txt","r")
+distance_synonyms=eval(input_file_handler.readline())
+print distance_synonyms
+input_file_handler.close()
+
+for i in distance_synonyms:
+	output_file_handler.write(i+"\n")
+
+
+input_file_handler.close()
+output_file_handler.close()
