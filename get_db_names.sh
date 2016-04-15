52d4fae944ec7cb9cae6d386dfba4088bf61c7dd
Chinmaya Gautam <chinmaya.gautam@gmail.com>
2016-04-15 15:21:42 -0700

    added list of names

--- get_db_names.sh
+++ get_db_names.sh
@@ -4,20 +4,19 @@
 #description     :This script will pull all the names from the database for NER
 #author          :chinmaya gautam
 #date            :04152016
-#version         :1.0.1    
+#version         :1.0.2    
 #usage           :sh get_db_names.sh
 #notes           :will only work with the database setup with GIS data for NLP project by anaphora resolution
 
 
+#to turn off tuple headers: \pset tuples_only
 /Library/PostgreSQL/9.5/bin/psql -d postgres -U postgres -p 5432 <<EOF
-\pset tuples_only
-SELECT name_engli FROM administrative;
-SELECT name_local FROM administrative;
-SELECT DISTINCT name_0 FROM administrative3;
-SELECT DISTINCT name_1 FROM administrative3;
-SELECT DISTINCT name_2 FROM administrative3;
-SELECT DISTINCT name_3 FROM administrative3;
-SELECT DISTINCT nam FROM water_lines;
-SELECT DISTINCT name FROM water_areas;
+SELECT name_engli AS country_name FROM administrative;
+SELECT name_local AS country_local_name FROM administrative;
+SELECT DISTINCT name_1 AS states FROM administrative3;
+SELECT DISTINCT name_2 AS cities FROM administrative3;
+SELECT DISTINCT name_3 AS cities FROM administrative3;
+SELECT DISTINCT nam AS rivers FROM water_lines;
+SELECT DISTINCT name AS lakes FROM water_areas;
 EOF
 
