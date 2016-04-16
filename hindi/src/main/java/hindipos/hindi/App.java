e08058a978f6f3d3731a97646c4afbe5f5d0d3ff
Harsh Fatepuria <fatepuri@usc.edu>
2016-04-15 18:15:05 -0700

    code and synonym dictionary update

--- hindi/src/main/java/hindipos/hindi/App.java
+++ hindi/src/main/java/hindipos/hindi/App.java
@@ -6,50 +6,57 @@ import in.ac.iitb.cfilt.jhwnl.data.IndexWord;
 import in.ac.iitb.cfilt.jhwnl.data.IndexWordSet;
 import in.ac.iitb.cfilt.jhwnl.data.Synset;
 import in.ac.iitb.cfilt.jhwnl.dictionary.Dictionary;
-
 import java.io.*;
+import java.util.*;
 
 import hindipos.translate.GoogleTranslateAPI;
 
-
-
-
-
 public class App 
 {
-	
-	
     public static void main( String[] args ) throws IOException
     {
-    	
+
+//		getSynonyms();
+		getSentenceDetails();
+//		useTranslate();
+    }
+    
+    public static void useTranslate()
+    {
     	JHWNL.initialize();
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    	GoogleTranslateAPI translator = new GoogleTranslateAPI("AIzaSyBEnSzexXv-Ve1E-d9rjHvygguF6rX9I8U");
+		String text = translator.translte("Greater Bombay", "en", "hi");
+		System.out.println(text);
+    }
+    
+    public static void getSentenceDetails() throws IOException
+    {
+    	JHWNL.initialize();
+    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		String inputLine;
 		try 
 		{
-				
 				//inputLine ="दूर";
 				inputLine=br.readLine();
 				
 				
-//				GoogleTranslateAPI translator = new GoogleTranslateAPI("AIzaSyBEnSzexXv-Ve1E-d9rjHvygguF6rX9I8U");
-//				String text = translator.translte("harsh", "en", "hi");
-//				System.out.println(text);
+				GoogleTranslateAPI translator = new GoogleTranslateAPI("AIzaSyBEnSzexXv-Ve1E-d9rjHvygguF6rX9I8U");
+				inputLine = translator.translte(inputLine, "en", "hi");
+			
 				
-
+				//print if not a valid question: इस जानकारी के लिए आपका धन्यवाद  (thank you for this information)
 				inputLine=inputLine.trim();
 				System.out.println("\nINPUT: " + inputLine+"\n\n");
 				
 				String[] splited = inputLine.split("\\s+");
 
-				
+//				System.out.println(splited.length);
 				for (int l=0; l<splited.length; l++)
 				{
 					System.out.println("WORD: "+splited[l]);
 					
 						//Look up the word for all POS tags
 						IndexWordSet demoIWSet = Dictionary.getInstance().lookupAllIndexWords(splited[l]);				
-						
 						//Note: Use lookupAllMorphedIndexWords() to look up morphed form of the input word for all POS tags				
 						IndexWord[] demoIndexWord = new IndexWord[demoIWSet.size()];
 						demoIndexWord  = demoIWSet.getIndexWordArray();
@@ -81,4 +88,80 @@ public class App
 			e.printStackTrace();
 		} 
     }
+    
+    
+    
+    
+    public static ArrayList<String> getSynonyms(String inputLine) throws IOException
+    {
+    	JHWNL.initialize();
+		int i;
+		ArrayList<String> syns=new ArrayList<String>();
+		try 
+		{
+				inputLine=inputLine.trim();
+//				System.out.println("\nINPUT: " + inputLine+"\n\n");
+				
+				
+//					System.out.println("WORD: "+inputLine);
+					
+						//Look up the word for all POS tags
+						IndexWordSet demoIWSet = Dictionary.getInstance().lookupAllIndexWords(inputLine);				
+						//Note: Use lookupAllMorphedIndexWords() to look up morphed form of the input word for all POS tags				
+						IndexWord[] demoIndexWord = new IndexWord[demoIWSet.size()];
+						demoIndexWord  = demoIWSet.getIndexWordArray();
+						
+						if(demoIndexWord.length==0)
+						{
+							System.out.println("No entry for this word in WordNet");
+						}
+						else
+						{
+							for ( i = 0;i < demoIndexWord.length;i++ ) 
+							{
+								int size = demoIndexWord[i].getSenseCount();
+								Synset[] synsetArray = demoIndexWord[i].getSenses(); 
+								for(int lll=0;  lll<200; lll++)
+								{
+									try{
+									
+								for ( int k = 0;k <= size;k++ ) 
+								{
+									try{
+										if(synsetArray[k].getWord(lll)!=null && syns.contains(synsetArray[k].getWord(lll).toString())==false)
+											syns.add(synsetArray[k].getWord(lll).toString());
+									}
+									catch(Exception e){
+										continue;
+									}
+									
+								}	
+									}
+									catch(Exception e){
+										break;
+									}
+								}
+							}
+							
+							
+//							System.out.print("[");
+//							
+//							for( i=0;i<syns.size()-1; i++ )
+//								System.out.print("\""+syns.get(i)+"\" , ");
+//							System.out.print("\""+syns.get(i)+"\"]");
+							
+							
+							
+						}
+						return syns;			
+		}
+		catch (JHWNLException e) 
+		{
+			System.err.println("Internal Error raised from API.");
+			e.printStackTrace();
+		} 
+		return syns;
+    }
+    
+    
 }
