4c80610e67b9fb7b737b49c51daee668ec181439
Rahul Agrawal <rahulagr2000@gmail.com>
2016-04-15 17:16:58 -0700

    generalized query property decider

--- RuleBasedSystem/rule.py
+++ RuleBasedSystem/rule.py
@@ -4,31 +4,15 @@ to be filled in the Postgre SQL Query
 '''
 
 import sys
+import time
 
-print "GEOGRAPHIC QUESTION ANSWERING SYSTEM"
-print "------------------------------------\n"
-print "Loading Query from file... "
 
-#filePath=sys.argv[1]
-
-#WINDOWS: filePath="C:\Users\rahul\Desktop\hindiCorpus.txt"
-#LINUX: filePath="C:/Users/rahul/Desktop/hindiCorpus.txt"
-
-filePath="hindiQuery.txt"
-
-input_file_handler= open(filePath,"r")
-output_file_handler= open("nlpOutput.txt","w")
-
-#query=input_file_handler.readline().strip().split()
-query=input_file_handler.readline().strip().split()
-#print query
-input_file_handler.close()
-
-for i in query:
-	output_file_handler.write(i+" ")
-
-#Function to check if query is a distance query
 def isPropertyDistance(query):
+	'''
+	Function to check if the query is a distance query or not
+	@param: query in list format, stripped and split
+	@return: True if a distance query, False otherwise
+	'''
 	input_file_handler= open("../synonyms/distance.txt","r")
 	distance_synonyms=eval(input_file_handler.readline())
 	#print distance_synonyms
@@ -40,6 +24,44 @@ def isPropertyDistance(query):
 		#output_file_handler.write("\n"+i)
 	if flag==1: return True
 
-print isPropertyDistance(query)
+
+
+def checkQueryProperty(query):
+	'''
+	Function to find the property of a query
+	@param: query in list format, stripped and split
+	@return: Type of query 
+	'''
+	if(isPropertyDistance(query)):
+		return 0
+	#if(isPropertyLength(query)):
+	#	return 1
+	#if(isPropertyArea(query)):
+	#	return 2
+	#if(isPropertyInside(query)):	
+	#	return 3
+
+
+
+
+print "GEOGRAPHIC QUESTION ANSWERING SYSTEM"
+print "------------------------------------\n"
+print "Loading Query from file... "
+time.sleep(2)
+#filePath=sys.argv[1]
+filePath="hindiQuery.txt"
+
+input_file_handler= open(filePath,"r")
+output_file_handler= open("nlpOutput.txt","w")
+
+query=input_file_handler.readline().strip().split()
+#print query
+input_file_handler.close()
+
+for i in query:
+	output_file_handler.write(i+" ")
+ 
+queryProperty = checkQueryProperty(query)
+print queryProperty
 
 output_file_handler.close()
